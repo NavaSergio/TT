38d9b8d5fd78f9ab5c9b7250db8799c03a61e585
NavaSergio <s3rgio.nava@gmail.com>
2025-01-07 16:19:41 -0600

    ahora se guardan las versiones de paquetes, R y Rstudio

--- ayudaconcdigor/temporal.R
+++ ayudaconcdigor/temporal.R
@@ -6,3 +6,42 @@ locq (1714, 79006, 879213, 15593224)
 data(Goettingen)
 locq (Goettingen$Goettingen2017[2:16], Goettingen$Goettingen2017[1], 
       Goettingen$BRD2017[2:16], Goettingen$BRD2017[1],  plot.results=TRUE)
+
+
+
+# Verificar si el paquete rstudioapi está instalado y cargarlo
+if (!require(rstudioapi)) install.packages("rstudioapi")
+
+# Obtener la versión de R
+version_r <- R.version$version.string
+
+# Obtener la versión de RStudio (si estás usando RStudio)
+version_rstudio <- if (rstudioapi::isAvailable()) {
+  rstudioapi::versionInfo()$version
+} else {
+  "No estás usando RStudio"
+}
+
+# Obtener las versiones de los paquetes instalados
+versiones_paquetes <- as.data.frame(installed.packages()[, c("Package", "Version")])
+
+# Guardar los resultados en un archivo
+archivo_salida <- "versiones_R_RStudio_paquetes.txt"
+
+writeLines(c(
+  paste("Versión de R:", version_r),
+  paste("Versión de RStudio:", version_rstudio),
+  "",
+  "Paquetes instalados y sus versiones:"
+), archivo_salida)
+
+write.table(
+  versiones_paquetes,
+  file = archivo_salida,
+  append = TRUE,
+  row.names = FALSE,
+  col.names = TRUE
+)
+
+# Mensaje de confirmación
+cat("Información guardada en", archivo_salida, "\n")
